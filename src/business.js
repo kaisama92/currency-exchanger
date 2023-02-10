998c0f3875958f1eb01916a2a66707b084ccad2a
Kai Clausen <kaiclausen123@gmail.com>
2023-02-10 11:35:05 -0800

    update business.js to handle errors.

--- src/business.js
+++ src/business.js
@@ -1,12 +1,20 @@
 import Exchange from './api.js';
-import { printExchange } from './index.js'
-import { printError } from './index.js';
+import { printExchange, printError } from './index.js';
 
-export function getAPIData(amount, isoCode) {
+export default function getAPIData(amount, isoCode) {
   Exchange.getExchange(amount, isoCode)
     .then(function(exchangeResponse) {
-      if (exchangeResponse instanceof Error) {
-        const errorMessage = `There was a problem accessing the exchange data from ExchangeRate-API for ${isoCode}`;
+      console.log(exchangeResponse);
+      if (!amount) {
+        const errorMessage = `Please enter an amount in the first input field`;
+        throw new Error(errorMessage);
+      } else if (exchangeResponse instanceof Error && isoCode === "") {
+        const errorMessage = `There was a problem accessing the exchange data from ExchangeRate-API for ${isoCode}: 
+        Please enter a valid Currency Code.`;
+        throw new Error(errorMessage);
+      } else if (exchangeResponse instanceof Error) {
+        const errorMessage = `There was a problem accessing the exchange data from ExchangeRate-API for ${isoCode}: 
+        ${exchangeResponse}`;
         throw new Error(errorMessage);
       }
       printExchange(exchangeResponse, isoCode, amount);
@@ -14,4 +22,4 @@ export function getAPIData(amount, isoCode) {
     .catch(function(error){
       printError(error);
     });
-  }
+}
