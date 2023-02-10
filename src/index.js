ca70b60bcadc8100e84bb82e615ad596398ecafc
Kai Clausen <kaiclausen123@gmail.com>
2023-02-10 15:16:25 -0800

    update styling

--- src/index.js
+++ src/index.js
@@ -7,14 +7,16 @@ import getAPIData from './business.js';
 // UI Logic 
 
 export function printExchange(response, isoCode1, isoCode2, amount) {
+  document.querySelector('.hidden').removeAttribute('class');
   let rate = parseFloat(response.conversion_rate);
   let result = parseFloat(response.conversion_result);
   let text = `The conversion rate from ${isoCode1} to ${isoCode2} is ${rate}.
-  Converting your ${amount} in ${isoCode1} to ${isoCode2} would result in ${(result).toFixed(2)} ${isoCode2}.`;
+  Converting ${amount} in ${isoCode1} to ${isoCode2} would result in ${(result).toFixed(2)} ${isoCode2}.`;
   document.querySelector('#results').innerText = text;
 }
 
 export function printError(error) {
+  document.querySelector('.hidden').removeAttribute('class');
   document.querySelector('#results').innerText = error;
 }
 
