bf3b9789a676252992580fef3f0ae4804d6663f0
Kai Clausen <kaiclausen123@gmail.com>
2023-02-10 09:09:41 -0800

    create handleFormSubmission()

--- src/index.js
+++ src/index.js
@@ -5,8 +5,17 @@ import './css/styles.css';
 
 // UI Logic 
 
+function handleFormSubmission(event){
+  event.preventDefault();
+  let amount = parseFloat(document.querySelector('#amount').value);
+  let isoCode = document.querySelector('#isoCode').value;
+  getExchange(amount, isoCode);
+}
+
+
+
 
 window.addEventListener("load", function() {
   const form = this.document.querySelector('form');
-  form.addEventListener('submit', handleSubmitForm);
+  form.addEventListener('submit', handleFormSubmission);
 })
