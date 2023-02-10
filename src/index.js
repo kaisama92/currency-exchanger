53fe5914628782aded49be05bff22101d0294147
Kai Clausen <kaiclausen123@gmail.com>
2023-02-10 11:35:41 -0800

    create printError().

--- src/index.js
+++ src/index.js
@@ -10,17 +10,22 @@ export function printExchange(response, isoCode, amount) {
   let rate = parseFloat(response.conversion_rate);
   let result = parseFloat(response.conversion_result);
   let text = `The conversion rate from USD to ${isoCode} is ${rate}.
-  Converting your ${amount} in USD to ${isoCode}would result in ${(result).toFixed(2)} ${isoCode}.`
+  Converting your ${amount} in USD to ${isoCode} would result in ${(result).toFixed(2)} ${isoCode}.`;
+  document.querySelector('#results').innerText = text;
+}
+
+export function printError(error) {
+  document.querySelector('#results').innerText = error;
 }
 
 function handleFormSubmission(event){
   event.preventDefault();
   let amount = parseFloat(document.querySelector('#amount').value);
-  let isoCode = document.querySelector('#isoCode').value;
+  let isoCode = document.querySelector('#isoCode').value.toUpperCase();
   getAPIData(amount, isoCode);
-};
+}
 
 window.addEventListener("load", function() {
   const form = this.document.querySelector('form');
   form.addEventListener('submit', handleFormSubmission);
-})
+});
